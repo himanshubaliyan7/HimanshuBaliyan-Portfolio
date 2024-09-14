eac66bfb4240486b0a27e4b5b07cd05051a1489a
Himanshu Baliyan <himanshubaliyan4000@gmail.com>
2024-09-15 03:10:24 +0530

    first commit

---
+++ src/sections/Footer.tsx
@@ -0,0 +1,48 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+
+const footerLinks = [
+  {
+    title: "LinkedIn",
+    href: "https://www.linkedin.com/in/himanshu-baliyan/",
+  },
+  {
+    title: "GitHub",
+    href: "https://github.com/your-github-username",
+  },
+  {
+    title: "Twitter",
+    href: "https://twitter.com/your-twitter-handle",
+  },
+  {
+    title: "Instagram",
+    href: "https://www.instagram.com/your-instagram-handle",
+  },
+];
+
+export const Footer = () => {
+  return (
+    <footer className="relative -z-10 overflow-x-clip">
+      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
+      <div className="container">
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row justify-between items-center gap-8">
+          <div className="text-white/40">
+            &copy; {new Date().getFullYear()}. All rights reserved.
+          </div>
+          <nav className="flex flex-col md:flex-row items-center gap-8">
+            {footerLinks.map((link) => (
+              <a
+                href={link.href}
+                key={link.title}
+                target="_blank"
+                className="inline-flex items-center gap-1.5"
+              >
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRightIcon className="size-4" />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
+};
