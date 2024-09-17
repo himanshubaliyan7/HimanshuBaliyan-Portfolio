e52f27be3438214624408a5ec0f1520f41a09c5c
Himanshu Baliyan <himanshubaliyan4000@gmail.com>
2024-09-17 23:25:03 +0530

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
-import {Inter, Calistoga} from "next/font/google"
+import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
 
-const inter = Inter({subsets: ['latin'], 
-                     variable: "--font-sans"});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
-const calistoga = Calistoga({subsets: ['latin'], 
-                             variable: "--font-serif", 
-                             weight: ["400"],});
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
 
 export const metadata: Metadata = {
   title: "Himanshu Baliyan",
@@ -17,16 +19,20 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly <{
+}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={twMerge(inter.variable,
-                               calistoga.variable,
-                               "bg-gray-900 text-white antialiased font-sans"
-            )}>
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          "bg-gray-900 text-white antialiased font-sans"
+        )}
+      >
         {children}
+        <Analytics />
       </body>
     </html>
   );
