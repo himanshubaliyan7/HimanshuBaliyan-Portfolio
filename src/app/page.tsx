079250f49be9d7dc004b85743ac4156ee85b3e09
Himanshu Baliyan <himanshubaliyan4000@gmail.com>
2024-09-17 22:42:29 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
-import { TestimonialsSection } from "@/sections/Testimonials";
 import { AboutSection } from "@/sections/About";
 import { ContactSection } from "@/sections/Contact";
 import { Footer } from "@/sections/Footer";
@@ -14,7 +13,6 @@ export default function Home() {
       <HeroSection />
       <ProjectsSection />
       <TapeSection />
-      <TestimonialsSection />
       <AboutSection />
       <ContactSection />
       <Footer />
